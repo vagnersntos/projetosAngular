72842ef014a458d73e56263f7d2287c17f90cad0
vagnersntos <vagner.sntos@gmail.com>
2022-03-25 21:55:14 -0300

    Uso da diretiva ngFor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { CInputFilhoComponent } from './c-input-filho/c-input-filho.component';
 import { CPaiComponent } from './c-pai/c-pai.component';
 import { OutputComponent } from './output/output.component';
 import { NgIfComponent } from './ng-if/ng-if.component';
+import { NgForComponent } from './ng-for/ng-for.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { NgIfComponent } from './ng-if/ng-if.component';
     CInputFilhoComponent,
     CPaiComponent,
     OutputComponent,
-    NgIfComponent
+    NgIfComponent,
+    NgForComponent
   ],
   imports: [
     BrowserModule,
